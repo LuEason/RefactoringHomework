7e3248551aab3599e640de4f592f8496f7870e84
LuEason <chenzscd@163.com>
2020-08-28 21:35:08 +0800

    [homework][Eason] refactor: extract methods handleVoyageProfitFactor

--- src/rank.js
+++ src/rank.js
@@ -36,7 +36,7 @@ function hasChina(history) {
   return history.some(v => 'china' === v.zone);
 }
 
-function captainHistoryRisk (voyage, history) {
+function captainHistoryRisk(voyage, history) {
   let result = 1;
   handleHistoryRisk.map(handler => {
     result += handler(voyage, history);
@@ -44,37 +44,39 @@ function captainHistoryRisk (voyage, history) {
   return Math.max(result, 0);
 }
 
-function voyageProfitFactor (voyage, history) {
+const handleVoyageProfitFactor = [
+  function (voyage, history) {
+    return COrEI.filter(c => voyage.zone === c).length;
+  },
+  function (voyage, history) {
+    return voyage.zone === 'china' && hasChina(history) ? 3 + handleVoyageProfitFactor[2](voyage, history) : handleVoyageProfitFactor[5](voyage, history);
+  },
+  function (voyage, history) {
+    return history.length > 10 ? 1 + handleVoyageProfitFactor[3](voyage, history) : handleVoyageProfitFactor[3](voyage, history);
+  },
+  function (voyage, history) {
+    return voyage.length > 12 ? 1 + handleVoyageProfitFactor[4](voyage, history) : handleVoyageProfitFactor[4](voyage, history);
+  },
+  function (voyage, history) {
+    return voyage.length > 18 ? -1 : 0;
+  },
+  function (voyage, history) {
+    return history.length > 8 ? 1 + handleVoyageProfitFactor[6](voyage, history) : handleVoyageProfitFactor[6](voyage, history);
+  },
+  function (voyage, history) {
+    return voyage.length > 14 ? -1 : 0;
+  }
+]
+
+function voyageProfitFactor(voyage, history) {
   let result = 2;
-  if (voyage.zone === 'china') {
-    result += 1;
-  }
-  if (voyage.zone === 'east-indies') {
-    result += 1;
-  }
-  if (voyage.zone === 'china' && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) {
-      result += 1;
-    }
-    if (voyage.length > 12) {
-      result += 1;
-    }
-    if (voyage.length > 18) {
-      result -= 1;
-    }
-  } else {
-    if (history.length > 8) {
-      result += 1;
-    }
-    if (voyage.length > 14) {
-      result -= 1;
-    }
+  for (let i = 0; i < 2; i++) {
+    result += handleVoyageProfitFactor[i](voyage, history);
   }
   return result;
 }
 
-function rating (voyage, history) {
+function rating(voyage, history) {
   const vpf = voyageProfitFactor(voyage, history);
   const vr = voyageRisk(voyage);
   const chr = captainHistoryRisk(voyage, history);
