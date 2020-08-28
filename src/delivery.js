028f80bebde52095bb1c5c0c432465516a68e6e7
LuEason <chenzscd@163.com>
2020-08-29 00:37:39 +0800

    [homework][Eason] refactor: extract methods handleIsRush

--- src/delivery.js
+++ src/delivery.js
@@ -1,24 +1,39 @@
-function deliveryDate (anOrder, isRush) {
-  if (isRush) {
-    let deliveryTime;
+const handleIsRush = [
+  function (deliveryState, deliveryTime) {
     if ([
       'MA',
       'CT',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
+    ].includes(deliveryState)) {
+      return 1;
     }
-    else if ([
+    return deliveryTime;
+  },
+  function (deliveryState, deliveryTime) {
+    if ([
       'NY',
       'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
+    ].includes(deliveryState)) {
+      return 2;
     }
-    else {
-      deliveryTime = 3;
+    return deliveryTime;
+  },
+  function (deliveryState, deliveryTime) {
+    if (deliveryTime === 0) {
+      return 3;
     }
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
+    return deliveryTime;
   }
-  else {
+]
+
+
+function deliveryDate(anOrder, isRush) {
+  if (isRush) {
+    let deliveryTime = 0;
+    handleIsRush.map(handler => {
+      deliveryTime = handler(anOrder.deliveryState, deliveryTime);
+    })
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+  } else {
     let deliveryTime;
     if ([
       'MA',
@@ -26,8 +41,7 @@ function deliveryDate (anOrder, isRush) {
       'NY',
     ].includes(anOrder.deliveryState)) {
       deliveryTime = 2;
-    }
-    else if ([
+    } else if ([
       'ME',
       'NH',
     ].includes(anOrder.deliveryState)) {
